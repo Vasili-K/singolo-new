4b8aefc18dfdb2e7ff5c38a59835afb39e7fa552
Vasili Kulakou <vasili-kulakov@yandex.ru>
2020-11-05 21:10:27 +0300

    fix: add some itemes

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+
+  console.log('hello body');
+
+  //navigation
+  addNavsClickHandler();
+
+}
+
+const addNavsClickHandler = () => {
+  document.querySelector('.navigation').addEventListener('click', (e) => {
+    if (e.target.classList.contains('navigation__link')) {
+      let clickedNav = e.target;
+      removeSelectedNavs();
+      selectClickedNav(clickedNav);
+    }
+  })
+}
+
+const removeSelectedNavs = () => {
+  let navs = document.querySelectorAll('.navigation .navigation__link');
+  navs.forEach(tag => {
+    tag.classList.remove('active');
+    tag.classList.add('unactive');
+  })
+}
+
+const selectClickedNav = (clickedNav) => {
+  clickedNav.classList.add('active');
+  clickedNav.classList.remove('unactive');
+}
