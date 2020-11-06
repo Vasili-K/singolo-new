36ad395ec4becb6f15cc0943a4f87f8d18838bb0
Vasili Kulakou <vasili-kulakov@yandex.ru>
2020-11-06 16:38:57 +0300

    feat: add interactivity
    Main navigation
    Portfilio tags
    Portfolio images (not finished)

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
+document.addEventListener('scroll', onscroll);
 
-window.onload = function() {
+//menu
 
-  console.log('hello body');
+function onscroll(e) {
+ const curPos = window.scrollY;
+ const divs =  document.querySelectorAll('#main-information>div');
+ const links = document.querySelectorAll('.navigation a');
 
-  //navigation
-  addNavsClickHandler();
-
+ divs.forEach((el) => {
+    if (el.offsetTop <= curPos && (el.offsetTop + el.offsetHeight) > curPos) {
+      links.forEach((a) => {
+        a.classList.remove('active');
+        if (el.getAttribute('class') === a.getAttribute('href').substring(1)) {
+          a.classList.add('active');
+        }
+      })
+    }
+  }); 
 }
 
-const addNavsClickHandler = () => {
-  document.querySelector('.navigation').addEventListener('click', (e) => {
-    if (e.target.classList.contains('navigation__link')) {
-      let clickedNav = e.target;
-      removeSelectedNavs();
-      selectClickedNav(clickedNav);
+//tags
+window.onload = function() {
+
+  addTagsClickHandler();
+
+}
+const addTagsClickHandler = () => {
+  document.querySelector('.portfolio-tag').addEventListener('click', (e) => {
+    if (e.target.classList.contains('tag-bordered')) {
+      let clickedTeg = e.target;
+      removeSelectedTags();
+      selectClickedTag(clickedTeg);
+      if (clickedTeg.innerText === 'All') {
+        showPictureFirstOrder();
+      } else {
+        changePictureOrder(clickedTeg.innerText);
+      }
     }
   })
 }
 
-const removeSelectedNavs = () => {
-  let navs = document.querySelectorAll('.navigation .navigation__link');
-  navs.forEach(tag => {
-    tag.classList.remove('active');
-    tag.classList.add('unactive');
+const  removeSelectedTags = () => {
+  let tags = document.querySelectorAll('.portfolio-tag span');
+  tags.forEach(span => {
+    span.classList.remove('colored');
   })
 }
 
-const selectClickedNav = (clickedNav) => {
-  clickedNav.classList.add('active');
-  clickedNav.classList.remove('unactive');
+const selectClickedTag = (clickedTeg) => {
+  clickedTeg.classList.add('colored');
+}
+
+const showPictureFirstOrder = () => {
+  let pictures = document.querySelectorAll('.picture-net img');
+  pictures.forEach(img => {
+    img.classList.remove('picture__hidden');
+  })  
+}
+
+const changePictureOrder = (selectedTag) => {
+  let pictures = document.querySelectorAll('.picture-net img');
+  pictures.forEach(img => {
+    img.classList.add('picture__hidden');
+
+  })
 }
